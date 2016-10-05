43ebef254bf55e5b6e87c0f78358d41dce1a92da
wallpocketsbyaviva <aviva.r.friedman@gmail.com>
2016-10-05 01:44:09 -0400

    commit 1

---
+++ Aviva Friedman/week2 HW/Homework 2.js
@@ -0,0 +1,46 @@
+console.log('yo js works');
+
+//put your code here
+
+
+
+var userplayer = prompt('Chose either Rock, Paper, or Scissors?')
+
+var computerplayer = math.random();
+
+if (computerplayer < .35); 
+{computerplayer = "rock";}
+
+else if (computerplayer >.34 && <.65)
+	{computerplayer = "paper";}
+
+else if (computerplayer >.64)
+	{computerplayer = "scissor";}
+
+else{
+	return ('error')
+}
+
+
+
+rock || paper || scissor
+
+
+
+var userscore = 0; 
+var compscore = 0; 
+
+
+var rockscore = 0;
+
+var paperscore = 0;
+
+var scissorscore = 0;  
+
+
+while(playing){
+  document.write('keep playing!'); 
+  playing = confirm ('Do you want to play again?')
+  console.log(playing); 
+}
+
